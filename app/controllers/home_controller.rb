01f7fbda4d8333bcdcf6abb8692a4e1f6d5146fb
daniel <danielpereira0069@gmail.com>
2025-09-29 19:26:25 -0300

    Adição da funcionalidade de busca de livros e filtro por categoria

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,23 @@
 class HomeController < ApplicationController
   layout 'public'
   def index
-    @books = Book.includes(:category).order(:title)
+    # Carrega todas as categorias para o dropdown do filtro
+    @categories = Category.all.order(:name)
+    
+    # Começa a busca por todos os livros
+    @books = Book.includes(:category)
+
+    # Filtra por TÍTULO se o parâmetro 'query' estiver presente
+    if params[:query].present?
+      @books = @books.where("title ILIKE ?", "%#{params[:query]}%")
+    end
+
+    # Filtra por CATEGORIA se o parâmetro 'category' estiver presente
+    if params[:category].present?
+      @books = @books.where(category_id: params[:category])
+    end
+
+    # Ordena os resultados pelo título
+    @books = @books.order(:title)
   end
 end
